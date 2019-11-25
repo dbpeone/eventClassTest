9bfefb8262b23d10c0a496c067366441f3cb5579
dbpeone <dbpeone@hotmail.com>
2019-11-24 17:49:49 -0800

    second1

--- eventClassTest/Form1.vb
+++ eventClassTest/Form1.vb
@@ -5,7 +5,7 @@ Public Class Form1
     Public WithEvents CoolFire As New ABFire()
 
     Private Sub coolFire_completed(e As String) Handles CoolFire.Action
-        MsgBox("finished " + e.ToString + "!!")
+        MsgBox("finished " + e.ToString + "! !")
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
